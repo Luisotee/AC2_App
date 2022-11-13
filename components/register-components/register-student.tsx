1aa348b681cc6f7e08f745ec4e82812d3b003fc7
Luisotee <luisotaviomgr1@gmail.com>
2022-11-13 05:35:02 -0300

    Register Aluno

--- components/register-components/register-student.tsx
+++ components/register-components/register-student.tsx
@@ -1,5 +1,21 @@
-import { Text } from "native-base";
+import { Button, HStack, Input, Text, TextArea, VStack } from "native-base";
 
-export function RegisterStudent() {
-  return <Text>Oi</Text>;
+export function RegisterStudent({ navigation }: any) {
+  return (
+    <VStack alignItems="center" space={4} mt="5">
+      <Input placeholder="Matricula" w="80%" />
+      <Input placeholder="Nome" w="80%" />
+      <TextArea placeholder="Endereço" w="80%" autoCompleteType={undefined} />
+      <Input placeholder="Cidade" w="80%" />
+      <Input placeholder="Foto (URL)" w="80%" />
+      <Button
+        mx="10"
+        mb="3"
+        size="lg"
+        onPress={() => navigation.navigate("Home")}
+      >
+        Submit
+      </Button>
+    </VStack>
+  );
 }
